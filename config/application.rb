e6326027bf3434457a967d74d01d51a8cd024e40
stephane cedroni <stephane.cedroni@gmail.com>
2013-12-04 10:43:36 -0500

    Initial commit

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,9 @@ module FinalApp
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Heroku precompile for rails 3
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
