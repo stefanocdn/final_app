7950c8c8d2bc08800952e669044cd7ff10e12d4c
stephane cedroni <stephane.cedroni@gmail.com>
2013-12-04 22:07:54 -0500

    Star registration structure

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.14'
 
+gem 'pg', '0.15.1'
+gem 'rails_12factor', '0.0.2'
+
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 # gem 'bootstrap-sass', '2.1'
@@ -23,11 +26,6 @@ gem 'rspec-rails', '2.11.0'
 # gem 'guard-rspec', '1.2.1'
 # gem 'guard-spork', '1.2.0'
 # gem 'spork', '0.9.2'
-end
-
-group :production do
-  gem 'pg', '0.15.1'
-  gem 'rails_12factor', '0.0.2'
 end
 
 # Gems used only for assets and not required
---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ActiveRecord::Base
+  attr_accessible :first_name, :last_name, :email, :birthday,
+   :password, :password_confirmation
+
+  has_secure_password
+
+  # Callbacks
+  before_save { email.downcase! }
+  before_save :create_remember_token
+
+  # Validations
+  validates :first_name,  presence: true, length: { maximum: 30 }
+  validates :last_name,  presence: true, length: { maximum: 30 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }, :unless => :password_not_required?
+  validates :password_confirmation, presence: true, :unless => :password_not_required?
+  after_validation { self.errors.messages.delete(:password_digest) }
+
+  def to_s
+    "#{first_name} #{last_name}"
+  end
+
+  private
+
+  def create_remember_token
+	self.remember_token = SecureRandom.urlsafe_base64
+  end
+
+  def password_not_required?
+	persisted? && @password.blank? && @password_confirmation.blank?
+  end
+end
