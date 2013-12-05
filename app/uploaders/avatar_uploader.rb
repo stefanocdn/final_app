30960c0ea0541cea14a7aeb6a7fab78278bb276c
stephane cedroni <stephane.cedroni@gmail.com>
2013-12-05 01:48:09 -0500

    change avatar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -28,7 +28,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Process files as they are uploaded:
-  process :resize_to_limit => [200, 200]
+  process :resize_to_fill => [200, 200]
   #
   # def scale(width, height)
   #   # do something
